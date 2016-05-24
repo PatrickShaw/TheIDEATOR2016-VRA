29387034dd0b8778d4ad8379057559e0d3d0cfb0
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2016-05-25 06:53:42 +1000

    Now properly using ActionBar colour

---
+++ app/src/main/java/com/ideator/ReportFragment.java
@@ -0,0 +1,23 @@
+package com.ideator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author Patrick Shaw (Patrick.Leong.Shaw@gmail.com)
+ * @since {25/05/2016}
+ */
+public class ReportFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_report, container, false);
+
+        return view;
+    }
+}
